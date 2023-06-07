9f864f517ced3de4b8a97cca8fa53f3097acd78c
marcelonv011 <marcelonv011@gmail.com>
2023-06-07 20:14:17 -0300

    criando outras entitys

--- src/model/Movimentacao.ts
+++ src/model/Movimentacao.ts
@@ -1,7 +1,20 @@
 import { AbstractEntity } from "./AbstractEntity";
+import { Condutor } from "./Condutor";
+import { Veiculo } from "./Veiculo";
 
 export class Movimentacao extends AbstractEntity{
 
-    
+    veiculo!: Veiculo
+    condutor!: Condutor
+    entrada!: Date
+    saida!: Date
+    tempo!: Date
+    tempoDesconto!: Date
+    tempoMulta!: Date
+    valorDesconto!: number
+    valorMulta!: number
+    valorTotal!: number
+    valorHora!: number
+    valorMinutoMulta!: number
 
 }
