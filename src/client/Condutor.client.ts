958ff73eed362f416eb3f2ac613de8af40d6bb04
marcelonv011 <marcelonv011@gmail.com>
2023-06-13 20:12:33 -0300

    config

---
+++ src/client/Condutor.client.ts
@@ -0,0 +1,62 @@
+import { Condutor } from "@/model/Condutor";
+import axios, { AxiosInstance } from "axios";
+
+export class CondutorClient {
+
+    private axiosClient: AxiosInstance
+
+    constructor(){
+        this.axiosClient = axios.create({
+            baseURL: 'http://localhost:8080/api/condutor',
+            headers: {
+                'Content-type' : 'application/json'
+            }
+        })
+    }
+
+    public async findById(id: number) : Promise<Condutor> {
+        try {
+            return (await this.axiosClient.get<Condutor>(`/${id}`)).data
+        }
+        catch(error:any){
+            return Promise.reject(error.response)
+        }
+    }
+
+    public async findAll() : Promise<Condutor[]> {
+        try {
+            return (await this.axiosClient.get<Condutor[]>(``)).data
+        }
+        catch(error:any){
+            return Promise.reject(error.response)
+        }
+    }
+
+    public async cadastrar(condutor: Condutor) : Promise<void> {
+        try {
+            return (await this.axiosClient.post(``, condutor)).data
+        }
+        catch(error:any){
+            return Promise.reject(error.response)
+        }
+    }
+
+    public async atualizar(condutor: Condutor) : Promise<void> {
+        try {
+            return (await this.axiosClient.put(`/${condutor.id}`, condutor)).data
+        }
+        catch(error:any){
+            return Promise.reject(error.response)
+        }
+    }
+
+    public async excluir(condutor: Condutor) : Promise<void> {
+        try {
+            return (await this.axiosClient.delete(`/${condutor.id}`)).data
+        }
+        catch(error:any){
+            return Promise.reject(error.response)
+        }
+    }
+
+}
