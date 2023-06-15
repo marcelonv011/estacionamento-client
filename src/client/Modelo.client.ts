c0e8d5fc09b6334720569dd5901b5828a1752bfc
marcelonv011 <marcelonv011@gmail.com>
2023-06-15 03:28:30 -0300

    modelo findbyid client

---
+++ src/client/Modelo.client.ts
@@ -0,0 +1,27 @@
+import { Modelo } from "@/model/Modelo";
+import axios, { AxiosInstance } from "axios";
+
+
+export class ModeloClient {
+
+    private axiosClient: AxiosInstance
+
+    constructor(){
+        this.axiosClient = axios.create({
+            baseURL: 'http://localhost:8080/api/modelo',
+            headers: {
+                'Content-type' : 'application/json'
+            }
+        })
+    }
+
+    public async findById(id: number) : Promise<Modelo> {
+        try {
+            return (await this.axiosClient.get<Modelo>(`?id=${id}`)).data
+        }
+        catch(error:any){
+            return Promise.reject(error.response)
+        }
+    }
+
+}
