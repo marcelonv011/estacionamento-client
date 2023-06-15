93ccfca15f3a23ce4f79c25a1a2ded4d015a3226
marcelonv011 <marcelonv011@gmail.com>
2023-06-14 23:47:55 -0300

    adicionando findbyid

---
+++ src/client/Marca.client.ts
@@ -0,0 +1,30 @@
+import { Marca } from "@/model/Marca";
+import axios, { AxiosInstance } from "axios";
+
+ 
+
+ export class MarcaClient {
+
+    private axiosClient : AxiosInstance
+
+    constructor(){
+        this.axiosClient = axios.create({
+            baseURL: 'http://localhost:8080/api/marca',
+            headers: {
+                'Content-Type' : 'application/json'
+            }
+        })
+    }
+
+    public async findById(id: number) : Promise<Marca> {
+        try{
+            return(await this.axiosClient.get<Marca>(`?id=${id}`)).data
+        }
+        catch(error:any){
+            return Promise.reject(error.response)
+        }
+    }
+
+    
+}
+ 
