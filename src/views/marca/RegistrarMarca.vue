fc64f2815104eefbac58c34dda1c774ae90c9aaf
marcelonv011 <marcelonv011@gmail.com>
2023-06-22 12:30:18 -0300

    cadastrar marca

--- src/views/marca/RegistrarMarca.vue
+++ src/views/marca/RegistrarMarca.vue
@@ -2,12 +2,43 @@
   <form class="d-flex flex-column align-items-center">
     <div class="mt-4 input-container">
       <label class="form-label">Nome</label>
-      <input class="form-control" placeholder="" />
+      <input class="form-control" placeholder="" v-model="marca.nome" />
     </div>
-    <button type="submit" class="btn btn-success mt-4">Cadastrar</button>
+    <button
+      type="button"
+      class="btn btn-success mt-4"
+      @click="onClickCadastrar"
+    >
+      Cadastrar
+    </button>
   </form>
 </template>
 
-<script lang="ts"></script>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+import MarcaClient from "@/client/MarcaClient";
+import { Marca } from "@/model/Marca";
+
+export default defineComponent({
+  name: "MarcaFormulario",
+  data() {
+    return {
+      marca: new Marca(),
+    };
+  },
+  methods: {
+    onClickCadastrar() {
+      MarcaClient.cadastrar(this.marca)
+        .then((sucess) => {
+          this.marca = new Marca();
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+});
+</script>
 
 <style lang="scss"></style>
